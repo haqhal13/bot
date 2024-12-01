1d36a652847de7a641a0736678731d389dfb0a5a
haqhal13 <haqhaleem06@gmail.com>
2024-12-01 10:07:33 +0000

    Add files via upload

--- bot.py
+++ bot.py
@@ -4,9 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
+def uptime_home():
+    """Root route to confirm the bot is live."""
+    return "Bot is active at root!", 200
+
+@app.route("/ping", methods=["GET", "HEAD"])
 def uptime_ping():
     """UptimeRobot Ping Endpoint"""
-    return "Bot is active!", 200
+    return "Bot is active at ping!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
