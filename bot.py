d0b75929f1d6f98be39562a75bd1d1efe5e85b3d
haqhal13 <haqhaleem06@gmail.com>
2024-12-20 19:03:09 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,10 @@ ADMIN_CHAT_ID = 834523364  # Replace with the admin's chat ID
 
 # Payment Information
 PAYMENT_INFO = {
-    "shopify": "https://bot-1-f2wh.onrender.com/pay-now/{plan_type}",
+    "shopify": {
+        "1_month": "https://5fbqad-qz.myshopify.com/cart/9925739086170:1",
+        "lifetime": "https://5fbqad-qz.myshopify.com/cart/50160363766106:1",
+    },
     "crypto": {"eth": "0x9ebeBd89395CaD9C29Ee0B5fC614E6f307d7Ca82"},
     "paypal": "onlyvipfan@outlook.com",
 }
